889e5211a1998ec921fdc288d7909c0f36535222
hayyantahirr <hayyantahir7@gmail.com>
2026-08-21 15:40:52 +0500

    user login and sign up has been completed

--- app.js
+++ app.js
@@ -370,5 +370,45 @@ const register = async () => {
     } else {
       alert(registerDataResponse.message);
     }
-  } catch (error) {}
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const login = async () => {
+  const chefEmail = document.querySelector("#loginEmail");
+  const chefPassword = document.querySelector("#loginPassword");
+
+  const emailValue = chefEmail ? chefEmail.value.trim() : "";
+  const passwordValue = chefPassword ? chefPassword.value : "";
+
+  if (!emailValue || !passwordValue) {
+    alert("Required fields are missing!");
+    return;
+  }
+
+  if (!emailRegex.test(emailValue)) {
+    alert("Invalid email address!");
+    return;
+  }
+
+  const loginData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  try {
+    const loginDataResponse = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    });
+
+    const data = await loginDataResponse.json();
+    console.log("data", data);
+  } catch (error) {
+    alert(error.message);
+  }
 };
